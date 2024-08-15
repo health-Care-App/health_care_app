55a822dab6cf90c376e6b9106a8113773ae8efd4
suisan0731 <suisan0731@gmail.com>
2024-08-15 19:40:21 +0900

    modified go dockerfile

--- backend/main.go
+++ backend/main.go
@@ -12,21 +12,5 @@ type Result struct {
 }
 
 func main() {
-    engine:= gin.Default()
-    engine.GET("/", func(c *gin.Context) {
-        dsn := "host=db user=healthCareUser password=healthCarePassword dbname=healthCareDB port=5432 sslmode=disable TimeZone=Asia/Tokyo"
-	    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	    if err != nil {
-		    fmt.Println("DB error(Init): ", err)
-	    }
-
-        // レコードを1行取得
-        var result Result
-        db.First(&result)
-
-        c.JSON(http.StatusOK, gin.H{
-            "test" : result.Name,
-        })
-    })
-    engine.Run(":8080")
+    fmt.Println("hello world")
 }
